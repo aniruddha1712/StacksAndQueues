1e2ff675eb9b26775ec4a0f4442c35c5a4028336
Aniruddha <aniruddha1712@gmail.com>
2022-04-10 17:41:32 +0530

    [Aniruddha]Added: Enqueue method in Queue class

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -12,13 +12,28 @@ namespace StacksAndQueues
         {
             Console.WriteLine("Welcome to the Stacks And Queues Program\n");
 
-            Stack stacks = new Stack();
-            stacks.Push(70);
-            stacks.Push(30);
-            stacks.Push(56);
-            stacks.Display();
-            stacks.IsEmopty();
-            stacks.Display();
+            Console.WriteLine("Choose\n 1:Stack\n 2:Queue\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Stack stacks = new Stack();
+                    stacks.Push(70);
+                    stacks.Push(30);
+                    stacks.Push(56);
+                    stacks.Display();
+                    stacks.IsEmopty();
+                    stacks.Display();
+                    break;
+                case 2:
+                    Queue queues = new Queue();
+                    queues.Enqueue(56);
+                    queues.Enqueue(30);
+                    queues.Enqueue(70);
+                    queues.Display();
+                    break;
+            }
+            
 
             Console.ReadLine();
         }
---
+++ StacksAndQueues/Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class Queue
+    {
+        public Node head;
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+            Console.WriteLine("{0} is inserted into Queue", newNode.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("stack is empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
